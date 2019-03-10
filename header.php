3bbcf00c68f03b8da0cb5560d0646c10e2386344
Annabelle Backman <abackman.43rd@gmail.com>
2019-03-10 03:37:11 +0000

    Simplified navigation header, branding.

--- header.php
+++ header.php
@@ -8,22 +8,17 @@
 <body <?php body_class(); ?>>
     <header id="main-header">
         <nav class="nav-menu-container">
-            <div class="nav-home-container">
-                <a href="<?php bloginfo('url'); ?>" class="nav-home">
-                    <?php bloginfo('name'); ?>
-                </a>
-            </div>
+            <?php if (has_custom_logo()): ?>
+            <div class="nav-home-container site-logo-container"><?php the_custom_logo(); ?></div>
+            <?php else: ?>
+            <h1 class="nav-home-container site-name">
+                <a class="title-link" href="<?php bloginfo('url'); ?>"><?php bloginfo('name'); ?></a>
+            </h1>
+            <?php endif; ?>
             <?php
             if (has_nav_menu('top-nav')) {
                 reorg_nav_menu('top-nav');
             }
             ?>
         </nav>
-        <?php if (has_custom_logo()): ?>
-        <div class="site-logo-container"><?php the_custom_logo(); ?></div>
-        <?php else: ?>
-        <h1 class="site-name">
-            <a class="title-link" href="<?php bloginfo('url'); ?>"><?php bloginfo('name'); ?></a>
-        </h1>
-        <?php endif; ?>
     </header>
